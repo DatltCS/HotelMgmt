ba03c4e9ec1d904e076418465d059b17cdf2fbf8
Linh Vu <prodige112002@gmail.com>
2022-11-18 10:03:41 +0700

    add database

---
+++ db_hotel_new.sql
@@ -0,0 +1,339 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hotel-mgmt
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `accounts` (
+  `UserID` varchar(12) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `UserPass` varchar(16) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `AccountType` enum('customer','employer') COLLATE utf8mb3_unicode_ci NOT NULL,
+  `AccountStatus` enum('active','close','blacklisted') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'active',
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `UserID_UNIQUE` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bill`
+--
+
+DROP TABLE IF EXISTS `bill`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bill` (
+  `BillID` int NOT NULL AUTO_INCREMENT,
+  `BookingID` int NOT NULL,
+  `CreateDate` date NOT NULL,
+  `Total` decimal(18,0) NOT NULL,
+  PRIMARY KEY (`BillID`),
+  KEY `fk_booking_bill_idx` (`BookingID`),
+  CONSTRAINT `fk_booking_bill` FOREIGN KEY (`BookingID`) REFERENCES `booking` (`BookingID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bill`
+--
+
+LOCK TABLES `bill` WRITE;
+/*!40000 ALTER TABLE `bill` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bill` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `booking`
+--
+
+DROP TABLE IF EXISTS `booking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking` (
+  `BookingID` int NOT NULL AUTO_INCREMENT,
+  `CreateDate` timestamp(2) NOT NULL,
+  `StartDate` date NOT NULL,
+  `EndDate` date NOT NULL,
+  `Status` enum('booked','checked-in','checked-out','canceled','abandoned') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'booked',
+  `CustomerID` int NOT NULL,
+  PRIMARY KEY (`BookingID`),
+  KEY `fk_booking_customers1_idx` (`CustomerID`),
+  CONSTRAINT `fk_booking_customers1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking`
+--
+
+LOCK TABLES `booking` WRITE;
+/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `booking_food`
+--
+
+DROP TABLE IF EXISTS `booking_food`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking_food` (
+  `BookingID` int NOT NULL,
+  `FoodID` int NOT NULL,
+  `Quantity` int NOT NULL,
+  `TotalBill` decimal(18,0) NOT NULL,
+  PRIMARY KEY (`BookingID`,`FoodID`),
+  KEY `fk_booking_has_food_food1_idx` (`FoodID`),
+  KEY `fk_booking_has_food_booking1_idx` (`BookingID`),
+  CONSTRAINT `fk_booking_has_food_booking1` FOREIGN KEY (`BookingID`) REFERENCES `booking` (`BookingID`),
+  CONSTRAINT `fk_booking_has_food_food1` FOREIGN KEY (`FoodID`) REFERENCES `food` (`FoodID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking_food`
+--
+
+LOCK TABLES `booking_food` WRITE;
+/*!40000 ALTER TABLE `booking_food` DISABLE KEYS */;
+/*!40000 ALTER TABLE `booking_food` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `booking_room`
+--
+
+DROP TABLE IF EXISTS `booking_room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking_room` (
+  `BookingID` int NOT NULL,
+  `RoomID` int NOT NULL,
+  PRIMARY KEY (`BookingID`,`RoomID`),
+  KEY `fk_booking_has_room_room1_idx` (`RoomID`),
+  KEY `fk_booking_has_room_booking1_idx` (`BookingID`),
+  CONSTRAINT `fk_booking_has_room_booking1` FOREIGN KEY (`BookingID`) REFERENCES `booking` (`BookingID`),
+  CONSTRAINT `fk_booking_has_room_room1` FOREIGN KEY (`RoomID`) REFERENCES `room` (`RoomID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking_room`
+--
+
+LOCK TABLES `booking_room` WRITE;
+/*!40000 ALTER TABLE `booking_room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `booking_room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `booking_services`
+--
+
+DROP TABLE IF EXISTS `booking_services`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `booking_services` (
+  `BookingID` int NOT NULL,
+  `ServiceID` int unsigned NOT NULL,
+  PRIMARY KEY (`BookingID`,`ServiceID`),
+  KEY `fk_booking_has_services_services1_idx` (`ServiceID`),
+  KEY `fk_booking_has_services_booking1_idx` (`BookingID`),
+  CONSTRAINT `fk_booking_has_services_booking1` FOREIGN KEY (`BookingID`) REFERENCES `booking` (`BookingID`),
+  CONSTRAINT `fk_booking_has_services_services1` FOREIGN KEY (`ServiceID`) REFERENCES `services` (`ServiceID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking_services`
+--
+
+LOCK TABLES `booking_services` WRITE;
+/*!40000 ALTER TABLE `booking_services` DISABLE KEYS */;
+/*!40000 ALTER TABLE `booking_services` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `CustomerID` int NOT NULL AUTO_INCREMENT,
+  `UserID` varchar(12) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `LastName` varchar(80) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `FirstName` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Gender` varchar(45) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `Birthday` date NOT NULL,
+  `PhoneNum` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Email` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `Address` varchar(100) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`CustomerID`),
+  KEY `fk_customers_accounts_idx` (`UserID`),
+  CONSTRAINT `fk_customers_accounts` FOREIGN KEY (`UserID`) REFERENCES `accounts` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `employees` (
+  `EmployeeID` int NOT NULL AUTO_INCREMENT,
+  `UserID` varchar(12) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `LastName` varchar(80) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `FirstName` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Gender` varchar(45) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `Birthday` date NOT NULL,
+  `PhoneNum` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Email` varchar(45) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `Address` varchar(45) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`EmployeeID`),
+  UNIQUE KEY `EmployeeID_UNIQUE` (`EmployeeID`),
+  KEY `fk_employees_accounts` (`UserID`),
+  CONSTRAINT `fk_employees_accounts` FOREIGN KEY (`UserID`) REFERENCES `accounts` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `food`
+--
+
+DROP TABLE IF EXISTS `food`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `food` (
+  `FoodID` int NOT NULL AUTO_INCREMENT,
+  `FoodName` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `Category` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `PricePerUnit` decimal(18,0) NOT NULL,
+  `UnitType` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `IsDeleted` date DEFAULT NULL,
+  PRIMARY KEY (`FoodID`),
+  UNIQUE KEY `FoodID_UNIQUE` (`FoodID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food`
+--
+
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `room`
+--
+
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `room` (
+  `RoomID` int NOT NULL AUTO_INCREMENT,
+  `PricePerDay` double NOT NULL,
+  `AllowSmoking` tinyint NOT NULL,
+  `SingleBed` int DEFAULT NULL,
+  `DoubleBed` int DEFAULT NULL,
+  `Status` enum('available','reserved','occupied','other') COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'available',
+  PRIMARY KEY (`RoomID`),
+  UNIQUE KEY `RoomID_UNIQUE` (`RoomID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `room`
+--
+
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `services`
+--
+
+DROP TABLE IF EXISTS `services`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `services` (
+  `ServiceID` int unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(45) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `ChargePerHour` decimal(18,0) NOT NULL,
+  `IsDeleted` date DEFAULT NULL,
+  PRIMARY KEY (`ServiceID`),
+  UNIQUE KEY `ServiceID_UNIQUE` (`ServiceID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `services`
+--
+
+LOCK TABLES `services` WRITE;
+/*!40000 ALTER TABLE `services` DISABLE KEYS */;
+/*!40000 ALTER TABLE `services` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-18  9:59:59
