62e8bb11342289278c7d990d3f966a246896f60f
Linh Vu <prodige112002@gmail.com>
2022-11-19 17:10:19 +0700

    add login fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.linhvu.hotelmgmt {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.linhvu.hotelmgmt to javafx.fxml;
     exports com.linhvu.hotelmgmt;
