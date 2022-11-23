978f8cc3a5c2e27cd604d6af2eb8f090d0083d4c
Linh Vu <prodige112002@gmail.com>
2022-11-23 16:13:14 +0700

    add packet, config

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.linhvu.hotelmgmt {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens com.linhvu.hotelmgmt to javafx.fxml;
     exports com.linhvu.hotelmgmt;
+//    exports com.linhvu.pojo;
 }
