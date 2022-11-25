4fe6aae7d03b13117ef9360ef869c50fcba75d5c
Linh Vu <prodige112002@gmail.com>
2022-11-25 13:38:24 +0700

    add interface 57

--- src/main/java/com/linhvu/hotelmgmt/App.java
+++ src/main/java/com/linhvu/hotelmgmt/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("FLogin"));
+        scene = new Scene(loadFXML("FMainEmployee"));
         stage.setScene(scene);
         stage.show();
     }
