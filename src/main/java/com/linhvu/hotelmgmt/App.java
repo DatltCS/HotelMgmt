db0987ed71f4bdc93a07e449720d131381107390
Linh Vu <prodige112002@gmail.com>
2022-11-30 09:29:12 +0700

    clean change scene

--- src/main/java/com/linhvu/hotelmgmt/App.java
+++ src/main/java/com/linhvu/hotelmgmt/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("FMainEmployee"));
+        scene = new Scene(loadFXML("FLogin"));
         stage.setScene(scene);
         stage.setTitle("Hotel del Luna Management App");
         stage.show();
