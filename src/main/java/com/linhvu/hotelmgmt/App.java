a2064a699dcebfd51bf65ea7b23e7a2c87b5f972
Linh Vu <prodige112002@gmail.com>
2022-12-05 13:21:11 +0700

    update readme

--- src/main/java/com/linhvu/hotelmgmt/App.java
+++ src/main/java/com/linhvu/hotelmgmt/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("FMainGuest"));
+        scene = new Scene(loadFXML("FLogin"));
         stage.setScene(scene);
         stage.setTitle("Hotel del Luna Management App");
         stage.show();
